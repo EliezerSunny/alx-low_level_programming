c30938d1e25d708adf51d4747ae77a18b58ffdc4
Adetunjieliazer <adetunjieliazer@gmail.com>
2023-09-07 00:53:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,4 @@
-#!/bin/bash
-int main() { puts("Programming is like building a multilingual puzzle"); return 0; }
+int main() {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
