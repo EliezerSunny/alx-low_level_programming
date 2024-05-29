9973a0a919e65414a24d0cc2420c1652d82fc023
EliezerSunny <adetunjieliazer@gmail.com>
2024-05-29 13:23:37 -0700

    modified

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,5 @@
-i#!/bin/bash
-gcc -fPIC -c *.c
-gcc -shared -o liball.so *.o
+#!/bin/bash
+# This script creates a dynamic library called liball.so from all the .c files in the current directory
+
+# Compile all .c files into position-independent code and create the shared library
+gcc -Wall -Werror -Wextra -pedantic -std=c99 -shared -o liball.so -fPIC *.c
